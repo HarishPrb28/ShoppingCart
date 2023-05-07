b6ac6c933ea21c9b49294b1915b551d5a2d5e1e0
hardrock28 <50201375+hardrock28@users.noreply.github.com>
2023-05-07 20:14:10 +0530

    First Commit

---
+++ script.js
@@ -0,0 +1,8 @@
+//login page
+document.querySelector("#login-btn").addEventListener("click", () => {
+  window.location.href = "./login/index.html";
+});
+//signup page
+document.querySelector("#signup-btn").addEventListener("click", () => {
+  window.location.href = "./signup/index.html";
+});
